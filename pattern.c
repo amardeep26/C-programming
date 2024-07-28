4032b78d1003522ba794bf7500c2d660a79ea380
Amar Deep <148640155+amardeep26@users.noreply.github.com>
2024-07-28 22:37:40 +0530

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -5,10 +5,10 @@
 
 int main() 
 {
-
+//complex pattern
     int n;
     scanf("%d", &n);
-  	// Complete the code to print the pattern.
+  
       int min=0;
       for(int i= 1;i <=2*n-1; i++){
           for(int j =1; j<=2*n-1 ;j++){
