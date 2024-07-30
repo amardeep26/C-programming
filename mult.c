0a54f4729de17b2cf56c700d49e29ec65485a61e
Amar Deep <148640155+amardeep26@users.noreply.github.com>
2024-07-30 21:44:17 +0530

    Add files via upload

---
+++ mult.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+int main(){
+    int n,m,i,j,p,q;
+    int a1[n][m] , a2[p][q], mult[n][q];
+    printf("enter no. of rows:- ");
+    scanf("%d",&n);
+    printf("enter no. of columns:- ");
+    scanf("%d",&m);
+    
+    
+    printf("enter matrix \n");
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            scanf("%d",&a1[i][j]);
+        }
+    }
+    
+        printf("enter no. of rows :- ");
+        scanf("%d",&p);
+        printf("enter no. of columns :- ");
+        scanf("%d",&q);
+        
+        if(m==p){
+            printf("\nerror\n");}
+        else{    
+            printf("enter matrix \n ");
+            for(i=0;i<p;i++){
+            for(j=0;j<q;j++){
+                scanf("%d",&a2[i][j]);
+            }
+        }
+       
+       for(i=0;i<n;i++){
+        for(j=0;j<q;j++){
+            int sum;
+            for (int k=0;k<m;k++){
+                sum+= a1[i][k] * a2[k][j];
+            }
+            mult[i][j]=sum;
+            sum=0;
+        }
+       }
+    }
+    printf("\nmultiplication of matrix\n");
+    for(i=0;i<n;i++){
+        for(j=0;j<q;j++){
+            printf("%d\t",mult[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
