81dea9acceed5ef09fbb2172b14c7f53bd8e3106
Amar Deep <148640155+amardeep26@users.noreply.github.com>
2024-08-02 22:28:25 +0530

    Add files via upload

---
+++ digit_to_word.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d\n",&n);
+    switch(n){
+            case '1':
+            printf("one\n");
+            break;
+            case '2':
+            printf("two\n");
+            break; 
+            case '3':
+            printf("three\n");
+            break;
+            case '4':
+            printf("four\n");
+            break;
+            case '5':
+            printf("five\n");
+            break;
+            case '6':
+            printf("six\n");
+            break;
+            case '7':
+            printf("siven\n");
+            break;
+            case '8':
+            printf("eignt\n");
+            break;
+            case '9':
+            printf("nine\n");
+            break;
+            default:
+            printf("Greater then 9\n");
+            
+        }
+    return 0;
+}
