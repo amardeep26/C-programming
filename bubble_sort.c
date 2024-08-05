0c168c0325c51c94fdae3c9d9aba97fb3666b37e
Amar Deep <148640155+amardeep26@users.noreply.github.com>
2024-08-05 22:58:15 +0530

    Add files via upload

---
+++ bubble_sort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+main()
+{
+int i,j,num,temp;
+printf("Enter number of elements\n");
+scanf("%d",&num);
+int a[num];
+printf("Enter numbers\n");
+for(i=0;i<num;i++)
+{
+ scanf("%d",&a[i]);
+}
+
+for(i=0;i<num;i++)
+{
+ for(j=i+1;j<num;j++)
+ {
+  if(a[i]>a[j])
+  {
+   temp=a[i];
+   a[i]=a[j];
+   a[j]=temp;
+  }
+ }
+}
+printf("Ascending oreder of given numbers is\n");
+for(i=0;i<num;i++)
+{ 
+ printf("%d\n",a[i]);
+
+}
+return 0;
+
+}
