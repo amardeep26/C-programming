d81564b971abfafdf6aa8f917ec95060e4a5bf5e
Amar Deep <148640155+amardeep26@users.noreply.github.com>
2024-08-09 19:22:44 +0530

    Update check_palindrome_in_string.c

--- check_palindrome_in_string.c
+++ check_palindrome_in_string.c
@@ -5,13 +5,14 @@ int main(){
    char str1[100],str2[100]={0};
    int i,j=0;
    printf("enter string :- \n");
-   gets(str1);
+   fgets(str1,100,stdin);
 
    for(i=strlen(str1)-1;i>=0;i--){
     str2[j]=str1[i];
     j++;
    }
    str2[j]='\0';
+   printf("the reverse of the string is :- %s \n",str2); 
 
    if(strcmp(str1,str2)==0){
     printf("the given string is Palindrome\n");
