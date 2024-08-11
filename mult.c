522fc2f4c7784a1d32ae55ee1e6806b7d27f4cf6
Amar Deep <148640155+amardeep26@users.noreply.github.com>
2024-08-11 19:29:43 +0530

    Update mult.c

--- mult.c
+++ mult.c
@@ -1,26 +1,26 @@
 #include<stdio.h>
 int main(){
-    int n,m,i,j,p,q;
-    int a1[n][m] , a2[p][q], mult[n][q];
+    int n,m,i,j,p,q,flag=1;
+   
     printf("enter no. of rows:- ");
     scanf("%d",&n);
     printf("enter no. of columns:- ");
     scanf("%d",&m);
     
-    
+      int a1[n][m];
     printf("enter matrix 1st \n");
     for(i=0;i<n;i++){
         for(j=0;j<m;j++){
             scanf("%d",&a1[i][j]);
         }
     }
-    
+    do{
         printf("enter no. of rows :- ");
         scanf("%d",&p);
         printf("enter no. of columns :- ");
         scanf("%d",&q);
-        
-        if(m==p){
+        int a2[p][q], mult[n][q];
+        if(m!=p){
             printf("\nerror\n");}
         else{    
             printf("enter matrix 2nd \n ");
@@ -39,18 +39,23 @@ int main(){
             mult[i][j]=sum;
             sum=0;
         }
-       }
-    }printf("\nelement of 1st matrix\n);
+       }  int x;
+           printf("enter '1' to continue addition in above matrix OR '0' to exist :- ");
+ scanf("%d",&x);  
+            while(flag);
+        }
+    }
+    printf("\nelement of 1st matrix\n);
     for(i=0;i<n;i++){
         for(j=0;j<m;j++){
-            printf("%d,a1[i][j]);
+            printf("%d",a1[i][j]);
             }
         }   
 
      printf("\nelement of 2st matrix\n);
      for(i=0;i<p;i++){
         for(j=0;j<q;j++){
-            printf("%d,a2[i][j]);
+            printf("%d",a2[i][j]);
             }
         }   
             
