b0de4413153d4489d7406c107264c119f0dca344
Amar Deep <148640155+amardeep26@users.noreply.github.com>
2024-08-13 22:28:09 +0530

    Update calculator_for_matrix.c

--- calculator_for_matrix.c
+++ calculator_for_matrix.c
@@ -104,7 +104,7 @@ int main (){
              scanf("%d",&a2[i][j]);
             }
         }
-        //sub
+        //substraction of two matrix
        for(i=0;i<n;i++)
         {
            for(j=0;j<m;j++)
@@ -128,7 +128,7 @@ int main (){
                         printf("\n");
                   }
 
-                     printf("\nAddition of above matrices is\n");
+                     printf("\nSubstraction of above matrices is\n");
 
                        for(i=0;i<n;i++)
                        {
@@ -143,6 +143,85 @@ int main (){
       break;}
 
       case '3' :
+        int i,j,rows_1,col_1,rows_2,col_2,flag=1;
+         printf("Enter number of rows and columns of matrix 1\n");
+         scanf("%d %d",&rows_1,&col_1);
+         printf("Enter number of rows and columns of matrix 2\n");
+          scanf("%d %d",&rows_2,&col_2);
+
+         int a1[rows_1][col_1],a2[rows_2][col_2];
+
+           if(rows_1==rows_2&&col_1==col_2)
+        {
+  //Taking input for 1st matrix
+     printf("Enter Matrix 1\n");
+     for(i=0;i<rows_1;i++)
+    {
+    for(j=0;j<col_1;j++)
+    {
+   scanf("%d",&a1[i][j]);
+    }
+     }
+     //Taking input for 2nd matrix
+      printf("Enter Matrix 2\n");
+     for(i=0;i<rows_2;i++)
+    {
+    for(j=0;j<col_2;j++)
+    {
+   scanf("%d",&a2[i][j]);
+    }
+    }
+
+    //Printing given matrices
+     printf("Matrix 1  is\n");
+     for(i=0;i<rows_1;i++)
+    {
+    for(j=0;j<col_1;j++)
+    {
+   printf("%d\t",a1[i][j]);
+
+    }
+    printf("\n");
+    }
+
+    printf("Matrix 2  is\n");
+     for(i=0;i<rows_2;i++)
+    {
+    for(j=0;j<col_2;j++)
+    {
+   printf("%d\t",a2[i][j]);
+    }
+    printf("\n");
+    }
+      //Comparing of matrix
+    for(i=0;i<rows_1;i++)
+    {
+     for(j=0;j<col_1;j++)
+     {
+     if(a1[i][j]!=a2[i][j])
+     {
+        flag=0;
+        }
+     }
+    }
+
+    if(flag==1)
+    {
+     printf("Both matrices are equal\n");
+    }
+    else
+    {
+     printf("Both matrices are not equal\n");
+    }
+
+
+ }
+ else
+ {
+  printf("Matrices Can not be compared\n");
+ }
+
+}
             
        
        break;}
