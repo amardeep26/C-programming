ff1da1658582a864c429b7606e1e0bf930d0a60e
Amar Deep <148640155+amardeep26@users.noreply.github.com>
2024-08-25 19:48:17 +0530

    Update mult.c

--- mult.c
+++ mult.c
@@ -1,72 +1,75 @@
 #include<stdio.h>
 int main(){
-    int n,m,i,j,p,q,flag=1;
-   
+    int n,m,i,j,p,q,x;
+   do{
     printf("enter no. of rows:- ");
-    scanf("%d",&n);
-    printf("enter no. of columns:- ");
     scanf("%d",&m);
+    printf("enter no. of columns:- ");
+    scanf("%d",&n);
     
-      int a1[n][m];
+      int a1[m][n];
     printf("enter matrix 1st \n");
-    for(i=0;i<n;i++){
-        for(j=0;j<m;j++){
-            scanf("%d",&a1[i][j]");
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            scanf("%d",&a1[i][j]);
         }
     }
-    do{
+    
         printf("enter no. of rows :- ");
         scanf("%d",&p);
         
         printf("enter no. of columns :- ");
         scanf("%d",&q);
         
-        int a2[p][q], mult[n][q];
-        if(m!=p){
+        int a2[p][q], mult[m][q];
+        if(n!=p){
             printf("\nerror\n");}
         else{    
             printf("enter matrix 2nd \n ");
             for(i=0;i<p;i++){
             for(j=0;j<q;j++){
-                scanf("%d",&a2[i][j]");
+                scanf("%d",&a2[i][j]);
+               }
             }
-        }
        
-       for(i=0;i<n;i++){
-        for(j=0;j<q;j++){
-            int sum;
-            for (int k=0;k<m;k++){
-                sum+= a1[i][k] * a2[k][j];
+           for(i=0;i<m;i++){
+              for(j=0;j<q;j++){
+                int sum;
+                 for (int k=0;k<n;k++){
+                   sum+= a1[i][k] * a2[k][j];
+                 }
+                     mult[i][j]=sum;
+                     sum=0;
+                  }
+              }  
+ 
+         }
+    printf("\nelement of 1st matrix\n");
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            printf("%d\t",a1[i][j]);
             }
-            mult[i][j]=sum;
-            sum=0;
-        }
- //       }  int x;
- //           printf("enter '1' to continue addition in above matrix OR '0' to exist :- ");
- // scanf("%d",&x);  
- //            while(flag);
- //        }
-    }
-    printf("\nelement of 1st matrix\n);
-    for(i=0;i<n;i++){
-        for(j=0;j<m;j++){
-            printf("%d",a1[i][j]");
-            }
+          printf("\n");
         }   
 
-     printf("\nelement of 2st matrix\n);
+     printf("\nelement of 2st matrix\n");
      for(i=0;i<p;i++){
         for(j=0;j<q;j++){
-            printf("%d",a2[i][j]");
+            printf("%d\t",a2[i][j]);
             }
+          printf("\n");
         }   
             
     printf("\nmultiplication of matrix\n");
-    for(i=0;i<n;i++){
+    for(i=0;i<m;i++){
         for(j=0;j<q;j++){
-            printf("%d\t",mult[i][j]");
+            printf("%d\t",mult[i][j]);
         }
         printf("\n");
     }
+         printf("enter '1' to continue OR '0' to exist :- ");
+          scanf("%d",&x);  
+ }while(x);
+        
     return 0;
 }
