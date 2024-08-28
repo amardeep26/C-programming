11fce5adf94eded64f79f140df8e8cbc25d2a3b2
Amar Deep <148640155+amardeep26@users.noreply.github.com>
2024-08-28 20:52:50 +0530

    Update random_number.c

--- random_number.c
+++ random_number.c
@@ -6,9 +6,12 @@
 
 int main()
 {   
-    
+    // Use current time as a seed for random # generator
+   
    srand(time(0));
- 
+
+
+    // rand() generates a pseudo random # between 0 - 32,767
    int number1 = (rand() % 100) + 1;
    int number2 = (rand() % 100) + 1;
    int number3 = (rand() % 100) + 1;
@@ -17,7 +20,7 @@ int main()
    
     printf("\nRANDM NUMBER From 0 to 100 :- \n");
    
-
+   
    printf("\n%d\n", number1);
    printf("\n%d\n", number2);
    printf("\n%d\n", number3);
