26405c3e2d508683a4f20e86bb16778e9ac3c9bb
Amar Deep <148640155+amardeep26@users.noreply.github.com>
2024-08-29 20:44:53 +0530

    Update transpose.c

--- transpose.c
+++ transpose.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(){
-   int n,m;
-   printf("enter no. 0f rows-");
+   int n,m,x;
+   do{
+   printf("enter no. of rows-");
    scanf("%d",&n);
    printf("enter no. of columes-");
    scanf("%d",&m);
@@ -21,14 +22,16 @@ int main(){
    }//int temp=m;
   //     m=n;
   //     n=temp;
-
+printf("\nAfter transpose of above matrix\n");
   for(int i=0;i<m;i++){
    for(int j=0;j<n;j++){
       printf("%d ",brr[i][j]);
    }
    printf("\n");
   }
-
+      printf("enter '0' to exit OR enter '1' to continues transpose :- ");
+      scanf("%d",&x);
+   }while(x);
   return 0;
 
 }
