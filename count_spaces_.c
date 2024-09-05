dd9487b68cdefe6573df219ad4c0f756f7a79b1b
Amar Deep <148640155+amardeep26@users.noreply.github.com>
2024-09-05 22:43:23 +0530

    Update count_spaces_.c

--- count_spaces_.c
+++ count_spaces_.c
@@ -2,8 +2,9 @@
 #include<string.h>
 
 int main(){
-      int i,space=0;
+      int i,space=0,x;
       char str[100];
+      do{
       printf("Enter a sentance :- ");
       fgets(str,100,stdin);
 
@@ -13,6 +14,9 @@ int main(){
              space++;
         }
       }
-      printf("Number of spaces in above sentance is %d \n",space);
+      printf("Number of spaces in above sentance is :- %d \n",space);
+      printf("press '0' to end OR press '1' to continue :- ");
+            scanf("%d"&x);
+      }while(x);
       return 0;
 }
